9c3d70e235e0f851a3a193b4b2b19e2da014af5e
George Nash <georgen@quicinc.com>
2013-10-10 11:45:29 -0700

    ALLJOYN-2441 Improved the SCons Doxygen Builder
    Added a scanner to the Doxygen builder.  The emitter was doing work that the
    scanner should have been doing.  The code is almost the same just split up.
    
    The scanner is responsible for finding all of the files needed to build the
    documentation while the emitter is used to find the input and output files
    when the doxygen command is run. The emitter will not add files to the source
    targets. While the scanner will.
    
    Change-Id: Ie15ebd5513b50f68662b92652176b54de9452b2f

--- tools/scons/doxygen.py
+++ tools/scons/doxygen.py
@@ -63,18 +63,8 @@ def _doxygen_replace_start_hash_if_found(str, env):
     else:
         return str
 
-def _doxygen_emitter(target, source, env):    
-    # work around its Safe to use the Doxygen Builder by only specifying the 
-    # config file as the source however SCons auto generates what it thinks is
-    # the proper target.  The auto generated target is wrong if the config
-    # file is specified as the source and no target is specified. This will 
-    # remove the source config file as a target. And prevents circular
-    # dependencies
-    for t in target:
-        for s in source:
-            if s == t:
-                target.remove(s)
-
+def _doxygen_scanner(node, env, path):
+    source = []
     #Default file pattern from doxygen 1.8
     file_patterns = ['*.c', '*.cc', '*.cxx', '*.cpp', '*.c++', '*.java',
                     '*.ii', '*.ixx', '*.ipp', '*.i++', '*.inl', '*.idl',
@@ -97,19 +87,14 @@ def _doxygen_emitter(target, source, env):
     # all paths listed in the config file are relative to the config file
     config_file_path = '.'
 
-    if len(source) > 1:
-        print('scons: *** Only one doxygen config file may be specified for the doxygen Builder.')
-        print('           Only the first source file is parsed by the Doxygen Builder all other')
-        print('           source files are ignored.')
-
     config = ConfigParser.SafeConfigParser()
     try:
-        fp = open(str(source[0]), 'r')
+        fp = open(str(node), 'r')
         config.readfp(FakeSecHead(fp))
     finally:
         fp.close()
 
-    config_file_path = os.path.abspath(os.path.dirname(str(source[0])))
+    config_file_path = os.path.abspath(os.path.dirname(str(node)))
 
     recursive = False
     input = []
@@ -120,28 +105,6 @@ def _doxygen_emitter(target, source, env):
     example_path = []
     example_patterns = []
     example_recursive = False
-    output_directory = config_file_path
-    html_output = 'html'
-    latex_output = 'latex'
-
-    if config.has_option('doxy_config', 'output_directory'):
-        output_directory_from_config = config.get('doxy_config', 'output_directory')
-        output_directory_from_config = _doxygen_expand_environment_vars(output_directory_from_config, env)
-        output_directory_from_config = _doxygen_replace_start_hash_if_found(output_directory_from_config, env)
-        # relative paths are relative to the location of the config file
-        if not os.path.isabs(output_directory_from_config):
-            output_directory_from_config = os.path.join(config_file_path, output_directory_from_config)
-        if os.path.isdir(output_directory_from_config):
-            output_directory = output_directory_from_config
-
-    # Some versions of doxygen will fail to create the output directory if it does
-    # not already exist. And will report that the directory does not exist and
-    # cannot be created.  This will by pass this error by creating the dictionary
-    # for doxygen
-    if not os.path.exists(output_directory):
-        output_directory_from_config = config.get('doxy_config', 'output_directory')
-        output_directory_from_config = _doxygen_expand_environment_vars(output_directory_from_config, env)
-        env.Mkdir(output_directory_from_config)
 
     ############################################################################
     # PROCESS INPUT FILES
@@ -200,12 +163,15 @@ def _doxygen_emitter(target, source, env):
     if config.has_option('doxy_config', 'example_patterns'):
          example_patterns = config.get('doxy_config', 'example_patterns').replace('\\', '').split()
 
+    if example_patterns == []:
+        example_patterns = None
+
     if config.has_option('doxy_config', 'example_recursive'):
         if config.get('doxy_config', 'example_recursive') == 'YES':
             example_recursive = True
 
     if config.has_option('doxy_config', 'example_path'):
-        input = config.get('doxy_config', 'example_path').replace('\\', '').split()
+        example_path = config.get('doxy_config', 'example_path').replace('\\', '').split()
 
     # if the example_path don't add any files to the source list
     # if the example_path is not blank but example_patterns is blank then add all
@@ -217,8 +183,8 @@ def _doxygen_emitter(target, source, env):
             e = _doxygen_replace_start_hash_if_found(e, env)
             # relative paths are relative to the location of the config file
             if not os.path.isabs(e):
-                e = os.path.join(config_file_path, e)
-            if e and os.path.isdir(e):
+                e = os.path.normpath(os.path.join(config_file_path, e))
+            if os.path.isdir(e):
                 if example_recursive:
                     for root, dir, filenames in os.walk(e):
                         if example_patterns:
@@ -234,7 +200,7 @@ def _doxygen_emitter(target, source, env):
                                 if fnmatch.fnmatchcase(filename, e_pattern):
                                     source.append(os.path.abspath(os.path.join(e, filename)))
                         else:
-                            if os.path.isfile(filename):
+                            if os.path.isfile(os.path.join(e, filename)):
                                 source.append(os.path.abspath(os.path.join(e, filename)))
 
     ############################################################################
@@ -291,14 +257,6 @@ def _doxygen_emitter(target, source, env):
                      hef = os.path.join(config_file_path, hef)
                  if os.path.isfile(hef):
                      source.append(os.path.abspath(hef))
-        if config.has_option('doxy_config', 'html_output'):
-            html_output_from_config = config.get('doxy_config', 'html_output')
-            html_output_from_config = _doxygen_expand_environment_vars(html_output_from_config, env)
-            html_output_from_config = _doxygen_replace_start_hash_if_found(html_output_from_config, env)
-            if html_output_from_config: 
-                html_output = html_output_from_config
-            target.append(env.Dir(os.path.abspath(os.path.join(output_directory, html_output))))
-            env.Clean(source, env.Dir(os.path.abspath(os.path.join(output_directory, html_output))))
 
     ############################################################################
     # PROCESS IMAGE_PATH TAG
@@ -312,7 +270,7 @@ def _doxygen_emitter(target, source, env):
         image_path = _doxygen_replace_start_hash_if_found(image_path, env)
         if image_path:
             for filename in os.listdir(os.path.abspath(image_path)):
-                if os.path.isfile(filename):
+                if os.path.isfile(os.path.join(image_path, filename)):
                     source.append(os.path.abspath(os.path.join(image_path, filename)))
 
     ############################################################################
@@ -343,7 +301,7 @@ def _doxygen_emitter(target, source, env):
             latex_header = _doxygen_replace_start_hash_if_found(latex_header, env)
             # relative paths are relative to the location of the config file
             if not os.path.isabs(latex_header):
-                html_header = os.path.join(config_file_path, latex_header)
+                latex_header = os.path.join(config_file_path, latex_header)
             if os.path.isfile(latex_header):
                 source.append(os.path.abspath(latex_header))
         if config.has_option('doxy_config', 'latex_footer'):
@@ -365,14 +323,6 @@ def _doxygen_emitter(target, source, env):
                      lef = os.path.join(config_file_path, lef)
                  if os.path.isfile(lef):
                      source.append(os.path.abspath(lef))
-        if config.has_option('doxy_config', 'latex_output'):
-            latex_output_from_config = config.get('doxy_config', 'latex_output')
-            latex_output_from_config = _doxygen_expand_environment_vars(latex_output_from_config, env)
-            latex_output_from_config = _doxygen_replace_start_hash_if_found(latex_output_from_config, env)
-            if latex_output_from_config:
-                latex_output = latex_output_from_config
-            target.append(env.Dir(os.path.abspath(os.path.join(output_directory, latex_output))))
-            env.Clean(source, env.Dir(os.path.abspath(os.path.join(output_directory, latex_output))))
 
     # TODO list of tags that effect the source and target list that are not 
     #    processed. Since none of our config files currently use these tags not 
@@ -419,6 +369,97 @@ def _doxygen_emitter(target, source, env):
     #    GENERATE_TAGFILE
 
     # Debug print statments used while developing
+    #print "*** Doxygen Scanner ***"
+    #print "*** list of source files ***"
+    #for s in source:
+    #     print str(s)
+    #print "\n\n"any
+    return source
+
+def _doxygen_emitter(target, source, env):
+    # work around its Safe to use the Doxygen Builder by only specifying the
+    # config file as the source however SCons auto generates what it thinks is
+    # the proper target.  The auto generated target is wrong if the config
+    # file is specified as the source and no target is specified. This will
+    # remove the source config file as a target. And prevents circular
+    # dependencies
+    for t in target:
+        for s in source:
+            if s == t:
+                target.remove(s)
+
+    config_file_path = '.'
+    config = ConfigParser.SafeConfigParser()
+    try:
+        fp = open(str(source[0]), 'r')
+        config.readfp(FakeSecHead(fp))
+    finally:
+        fp.close()
+
+    config_file_path = os.path.abspath(os.path.dirname(str(source[0])))
+
+    generate_html = True
+    generate_latex = True
+
+    output_directory = config_file_path
+    html_output = 'html'
+    latex_output = 'latex'
+
+    if config.has_option('doxy_config', 'output_directory'):
+        output_directory_from_config = config.get('doxy_config', 'output_directory')
+        output_directory_from_config = _doxygen_expand_environment_vars(output_directory_from_config, env)
+        output_directory_from_config = _doxygen_replace_start_hash_if_found(output_directory_from_config, env)
+        # relative paths are relative to the location of the config file
+        if not os.path.isabs(output_directory_from_config):
+            output_directory_from_config = os.path.join(config_file_path, output_directory_from_config)
+        if os.path.isdir(output_directory_from_config):
+            output_directory = output_directory_from_config
+
+    # Some versions of doxygen will fail to create the output directory if it does
+    # not already exist. And will report that the directory does not exist and
+    # cannot be created.  This will by pass this error by creating the dictionary
+    # for doxygen
+    if not os.path.exists(output_directory):
+        output_directory_from_config = config.get('doxy_config', 'output_directory')
+        output_directory_from_config = _doxygen_expand_environment_vars(output_directory_from_config, env)
+        env.Mkdir(output_directory_from_config)
+
+    ############################################################################
+    # PROCESS HTML TAGS
+    ############################################################################
+    if config.has_option('doxy_config', 'generate_html'):
+        if config.get('doxy_config', 'generate_html') == 'NO':
+            generate_html = False
+
+    if generate_html:
+        if config.has_option('doxy_config', 'html_output'):
+            html_output_from_config = config.get('doxy_config', 'html_output')
+            html_output_from_config = _doxygen_expand_environment_vars(html_output_from_config, env)
+            html_output_from_config = _doxygen_replace_start_hash_if_found(html_output_from_config, env)
+            if html_output_from_config:
+                html_output = html_output_from_config
+            target.append(env.Dir(os.path.abspath(os.path.join(output_directory, html_output))))
+            env.Clean(source, env.Dir(os.path.abspath(os.path.join(output_directory, html_output))))
+
+    ############################################################################
+    # PROCESS LATEX / PDF input
+    ############################################################################
+    if config.has_option('doxy_config', 'generate_latex'):
+        if config.get('doxy_config', 'generate_latex') == 'NO':
+            generate_latex = False
+
+    if generate_latex:
+        if config.has_option('doxy_config', 'latex_output'):
+            latex_output_from_config = config.get('doxy_config', 'latex_output')
+            latex_output_from_config = _doxygen_expand_environment_vars(latex_output_from_config, env)
+            latex_output_from_config = _doxygen_replace_start_hash_if_found(latex_output_from_config, env)
+            if latex_output_from_config:
+                latex_output = latex_output_from_config
+            target.append(env.Dir(os.path.abspath(os.path.join(output_directory, latex_output))))
+            env.Clean(source, env.Dir(os.path.abspath(os.path.join(output_directory, latex_output))))
+
+    # Debug print statments used while developing
+    #print "*** Doxygen Emiter***"
     #print "*** list of source files ***" 
     #for s in source:
     #     print str(s)
@@ -427,15 +468,19 @@ def _doxygen_emitter(target, source, env):
     #for t in target:
     #     print str(t)
     #print "\n\n"
-    fp.close()
     return target, source
 
 def generate(env):
     # Add Builders for the Doxygen documentation tool
     import SCons.Builder
+    doxygen_scanner = env.Scanner(name = 'doxygen_scanner',
+                                  function = _doxygen_scanner)
+
     doxygen_builder = SCons.Builder.Builder(
         action = '${DOXYGENCOM}',
         emitter = _doxygen_emitter,
+        source_scanner = doxygen_scanner,
+        single_source = 1
     )
 
     env.Append(BUILDERS = {
